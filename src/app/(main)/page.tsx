178f0b966f725aa7e20765d13ebe9accafac8796
denimozh <denimozh@gmail.com>
2024-08-02 23:44:55 +0100

    Completed Follow Feature and Follow Feed

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -1,9 +1,10 @@
 import PostEditor from "@/components/posts/editor/PostEditor";
 import Post from "@/components/posts/Post";
 import prisma from "@/lib/prisma";
-import { postDataInclude } from "@/lib/types";
 import TrendSideBar from "./TrendSideBar";
 import ForYouFeed from "./ForYouFeed";
+import FollowingFeed from "./FollowingFeed";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
 
 export default async function Home() {
     
@@ -11,7 +12,18 @@ export default async function Home() {
       <main className="w-full min-w-0 flex gap-5">
         <div className="w-full min-w-0 space-y-5">
             <PostEditor/>
-            <ForYouFeed />
+            <Tabs defaultValue="for-you">
+              <TabsList>
+                <TabsTrigger value="for-you">For you</TabsTrigger>
+                <TabsTrigger value="following">Following</TabsTrigger>
+              </TabsList>
+              <TabsContent value="for-you">
+                <ForYouFeed />
+              </TabsContent>
+              <TabsContent value="following">
+                <FollowingFeed />
+              </TabsContent>
+            </Tabs>
         </div>
         <TrendSideBar/>
       </main>
