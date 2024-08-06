6346c150c646500b29891bfc06895a5267b10347
denimozh <denimozh@gmail.com>
2024-08-06 23:51:14 +0100

    Update Profile Mutation

--- src/app/api/users/username/[username]/mutations.ts
+++ src/app/api/users/username/[username]/mutations.ts
@@ -1,7 +1,10 @@
 import { useToast } from "@/components/ui/use-toast";
 import { useUploadThing } from "@/lib/uploadThing";
-import { useQueryClient } from "@tanstack/react-query";
+import { InfiniteData, QueryFilters, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
+import { UpdateUserProfileValues } from "@/lib/validation";
+import { updateUserProfile } from "./actions";
+import { PostsPage } from "@/lib/types";
 
 export function useUpdateProfileMutation(){
     const {toast} = useToast();
@@ -12,5 +15,23 @@ export function useUpdateProfileMutation(){
 
     const {startUpload: startAvatarUpload} = useUploadThing("avatar");
 
-    
+    const mutation = useMutation({
+        mutationFn: async ({values, avatar}: {values: UpdateUserProfileValues, avatar?: File}) => {
+            return Promise.all([
+                updateUserProfile(values),
+                avatar && startAvatarUpload([avatar])
+            ])
+        },
+        onSuccess: async ([updatedUser, uploadResult]) => {
+            const newAvatarUrl = uploadResult?.[0].serverData.avatarUrl;
+
+            const queryFilter: QueryFilters = {
+                queryKey: ["post-feed"]
+            }
+
+            await queryClient.cancelQueries(queryFilter);
+
+            queryClient.setQueriesData<InfiniteData<PostsPage, string | null>>()
+        }
+    })
 }
