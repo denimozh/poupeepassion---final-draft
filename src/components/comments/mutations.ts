3e55b3b5c550f041b25ca53d321de60c49dfb6a3
denimozh <denimozh@gmail.com>
2024-08-28 23:19:06 +0100

    Comment Feature Finished

--- src/components/comments/mutations.ts
+++ src/components/comments/mutations.ts
@@ -1,7 +1,7 @@
 import { InfiniteData, QueryKey, useMutation, useQueryClient } from "@tanstack/react-query";
 import { useToast } from "../ui/use-toast";
-import { submitComment } from "./actions";
-import { CommentsPage } from "@/lib/types";
+import { deleteComment, submitComment } from "./actions";
+import { CommentData, CommentsPage } from "@/lib/types";
 
 export function useSubmitCommentMutation(postId: string) {
     const { toast } = useToast();
@@ -56,5 +56,48 @@ export function useSubmitCommentMutation(postId: string) {
         },
     });
     
+    return mutation;
+}
+
+export function useDeleteCommentMutation() {
+    const { toast } = useToast();
+  
+    const queryClient = useQueryClient();
+
+    const mutation = useMutation({
+        mutationFn: deleteComment,
+        onSuccess: async (deletedComment) => {
+            const queryKey: QueryKey = ["comments", deletedComment.postId]
+
+            await queryClient.cancelQueries({ queryKey });
+
+            queryClient.setQueryData<InfiniteData<CommentsPage, string | null>>(
+                queryKey,
+                (oldData) => {
+                    if (!oldData) return;
+
+                    return {
+                        pageParams: oldData.pageParams,
+                        pages: oldData.pages.map((page) => ({
+                            previousCursor: page.previousCursor,
+                            comments: page.comments.filter((c) => c.id !== deletedComment.id),
+                        }))
+                    }
+                }
+            )
+
+            toast({
+                description: "Comment Deleted"
+            })
+        },
+        onError(error) {
+            console.error(error);
+            toast({
+              variant: "destructive",
+              description: "Failed to delete post. Please try again.",
+            });
+          },
+    });
+
     return mutation;
 }
